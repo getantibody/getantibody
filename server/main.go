0b2e28d78fb849a1f3ef48ca99f476c80ace1784
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-09-26 15:49:00 -0300

    created latest endpoint

--- server/main.go
+++ server/main.go
@@ -1,24 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/caarlos0/getantibody"
+	"github.com/labstack/echo"
+	mw "github.com/labstack/echo/middleware"
+	"github.com/caarlos0/env"
 )
 
+type Config struct {
+	Port string `env:"PORT" envDefault:"3000"`
+}
+
+const download = "https://github.com/caarlos0/antibody/releases/download/%s/antibody_%s_%s.tar.gz"
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		release, err := getantibody.LatestRelease()
+	var config Config
+	env.Parse(&config)
+
+	e := echo.New()
+	e.Use(mw.Logger())
+	e.Use(mw.Recover())
+
+	e.Get("/latest/:os/:arch", func(c *echo.Context) error {
+		os := c.Param("os")
+		arch := c.Param("arch")
+		v, err := getantibody.LatestRelease()
 		if err != nil {
-			w.WriteHeader(500)
-			return
+			return err
 		}
-		w.Write([]byte(release))
+		return c.Redirect(
+			http.StatusSeeOther,
+			fmt.Sprintf(download, v, os, arch),
+		)
 	})
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
-	http.ListenAndServe(":"+port, nil)
+
+	e.Run(":" + config.Port)
 }
